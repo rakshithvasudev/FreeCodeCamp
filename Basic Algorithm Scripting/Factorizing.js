48a75d9b6050c6265c697d6fe4000943143d40a6
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-08 11:29:51 -0700

    Added Factorial

---
+++ Basic Algorithm Scripting/Factorizing.js
@@ -0,0 +1,29 @@
+/**
+ Return the factorial of the provided integer.
+https://www.freecodecamp.com/challenges/factorialize-a-number
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Arithmetic Operators
+*/
+
+
+
+function factorialize(num) {
+  if(num===0)
+    return 1;
+  return num*factorialize(num-1);
+}
+
+factorialize(5);
+
+
+
