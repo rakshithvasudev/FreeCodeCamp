39db9a5181ab3cecc9cf0a1ee1ff3359e3787e2f
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-05-08 15:57:29 -0700

    Added string Truncating

--- Basic Algorithm Scripting/TruncateString.js
+++ Basic Algorithm Scripting/TruncateString.js
@@ -9,3 +9,14 @@ String.prototype.slice()
  *  
  */
 
+function truncateString(str, num) {
+   if(num<str.length && num>3)
+      return str.slice(0,num-3)+"...";
+   else if(num==str.length)   
+    return str;
+   else if(num>str.length)   
+    return str;
+  return str.slice(0,num)+"...";
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
